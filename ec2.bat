0e72cd09dd45645c15275368cb2c3d099136041b
kiransree26 <67421116+kiransree26@users.noreply.github.com>
2020-07-10 09:13:29 -0400

    Update ec2.bat

--- ec2.bat
+++ ec2.bat
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-05bb2dae0b1de90b3 --count 1 --instance-type t2.micro --key-name test --security-groups testsecgroup
+aws ec2 run-instances --image-id ami-0a83d9223efc49d62 --count 1 --instance-type t2.micro --key-name test --security-group-ids sg-0476114d5cdbc7cf1 --subnet-id subnet-1b5a9a70 --region us-east-2
